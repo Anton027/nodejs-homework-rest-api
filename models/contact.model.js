77570a327637fea354fb439b706b2fdc54e65a64
Anton Kutsim <kutsimanton@gmail.com>
2022-11-11 08:20:27 +0200

    finished

---
+++ models/contact.model.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const contactShema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Set name for contact'],
+    },
+    email: {
+        type: String,
+    },
+    phone: {
+        type: String,
+    },
+    favorite: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const Contact = model('contacts', contactShema);
+
+module.exports = {
+    Contact
+};
